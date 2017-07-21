57e23e26b88eee8122472f956c0244ae6e890d2b
ivanahepjuk <ivanahepjuk@gmail.com>
2017-07-21 13:03:06 +0200

    Exception when there is not a 0 as a first dice throw

--- scripts/page.js
+++ scripts/page.js
@@ -2,89 +2,37 @@
 Created by ivanahepjuk@gmail.com
 */
 
-
-var hexa = "";
+var hexal = "";
+//var hexadecimal = "";
 
 function klik(num) {
 
 	if (num === 1) 
-		hexa += "0";
+		hexal += "0";
 	if (num === 2)
-		hexa += "1";
+		hexal += "1";
 	if (num === 3)
-		hexa += "2";
+		hexal += "2";
 	if (num === 4)
-		hexa += "3";
+		hexal += "3";
 	if (num === 5)
-		hexa += "4";
+		hexal += "4";
 	if (num === 6)
-		hexa += "5";
+		hexal += "5";
 
-	document.getElementById("hexa").value=hexa;
+	document.getElementById("hexa").value=hexal;
 
-	//setting the progressbar NEMAZAT
-	/*
-	var progressbarsetting = "width: " + ((100 * hexa.length) / 84) + "%";
-	document.getElementById("progressbar").setAttribute("style", progressbarsetting);
-	*/
-
-
-
-
-
-
-
-
-
-	/*Zdar TOMASU!!
-	
-	Do promenne hexa se pri kazdem hodu plni string obsahujici cisla 0-5, pri kazdem kliku/hodu se ten string zvetsuje. 
-	Pokazde jak se kod dostane tady kde ted pisu komentar (pri kazdem hodu), je potreba vzit tento hexal string ktery je v sestkove soustave a 
-	udelat z nej string v 16kove soustave. V podmince ktera nasleduje o dva radky dojde k otestovani toho, jestli ten hexadecimal string uz ma delku 64 (klidne muze zcinat i nulou, to je jedno). 
-	Pokud uz je nasbirano v hodech z kostky dosttek cislic na sestaveni 64mistneho hexadecimalniho cisla, dojde ke generaci seedu. 
-	Zatim jak vidis tam je natvrdo hexadecimalni string dlouhy 64mist, ze ktereho se spocita seed po peti hodech kostkou.
-	Na tom pochopis jak to funguje.
-	*/
-
-    hexadec = hexal2hxd(hexa)
-    console.log(hexadec);
-
-
-	//tohle s "nelogickym" pouzitim promenne hexa je tady proto, aby si po peti kliknutich videl ze to funguje a lepe se zorientoval
-	//v tom jak to myslim
-	if(hexa.length >= 5)
-		document.getElementById("seed").value = mn_encode('41a80cd3707c6b8e015c95bbe73f2461de407ba5677cbd205d528a6eb89d7e0d', 'english');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-	//A tohle tady bude, az vyresis ten hexadecimal string,)
-	if (hexadecimal.length >= 64){
-		//pokud bude uzivatel klikat i po nahazeni dostatecne delky,
-		//dalsi hody kostkou se zahodi a timpadem nebudou mit viv
-		hexadecimal = hexadecimal.substring(0,64);
-
-		//tohle vygeneruje seed
-		document.getElementById("seed").value = mn_encode(hexadecimal, 'english');
+	if (hexal.length >= 100){		
+		hexal = hexal.substring(0,100);	
+		document.getElementById("seed").value = mn_encode(hexal2hxd(hexal), 'english');
 	}
-*/
-	
+
+	//setting the progressbar NEMAZAT	
+	//var progressbarsetting = "width: " + (hexal.length) + "%";
+	//document.getElementById("progressbar").setAttribute("style", progressbarsetting);
 }
 
-// addapted from
+// adapted from
 // https://stackoverflow.com/questions/18626844/convert-a-large-integer-to-a-hex-string-in-javascript
 function hexal2hxd(str){ // .toString(16) only works up to 2^53
     var hexal = str.toString().split(''), sum = [], hxd = [], i, s
